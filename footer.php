7415d8d9d34c2fd1185b5ba1b3278a5500388d69
Marion <jolym8@gmail.com>
2020-08-03 15:40:23 +0200

    ajout header et cookies

---
+++ footer.php
@@ -0,0 +1,35 @@
+
+<footer>
+
+</footer>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+
+
+<script src="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.js" data-cfasync="false"></script>
+<script>
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#aa0000",
+      "text": "#ffdddd"
+    },
+    "button": {
+      "background": "#ff0000"
+    }
+  },
+  "theme": "edgeless",
+  "content": {
+    "message": "Ce site utilise des cookies pour vous garantir la meilleure expérience sur notre site.",
+    "dismiss": "J'accepte",
+    "link": "En savoir plus"
+  }
+});
+</script>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+
+    <!-- Required meta tags -->
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />  
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="IE=7">
+    <meta name="author" content="Marion JOLY" />
+    <meta name="copyright" content="Collectif Idahot" />
+    <meta name="description" content="Le collectif inter-associatif IDAHOT (International Day Against Homophobia and Transphobia) rassemble les 6 associations amiénoises de lutte contre toutes les formes de discriminations et pour la défense des droits des femmes et des personnes Lesbiennes, Gays, Bisexuel.les, Transidentitaires, Queer, Inteersexes et plus (LGBTQI+)." />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">    
+    <!-- styles CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">      
+    <!-- Librairie Font awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">    
+    <!-- icone de l'onglet -->
+    <link rel="icon" href="assets/img/logo.ico" /> 
+    <!-- polices '?' et '?' -->
+
+    <!-- Cookies -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/cookieconsent@3/build/cookieconsent.min.css" />
+
+    <!-- Titre de l'onglet -->
+    <title>Pride Amiens</title>
+
+</head>
+
+<body>
+
+<div class="container-fluid">
+<header>
+
+    <div class="row">
+        <div class="col-sm-12 col-lg-12">
+<p>Hello world !</p>
+        </div>
+    </div>
+
+</header>
+
